aff9be663b6947d8cb062772f5077f2963ecda05
Jon Bell <github@lot23.com>
2025-05-20 19:12:01 +1200

    Initial commit: Set up Next.js template

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl font-semibold">
+        Hi, this is where the awesomeness will go
+      </h1>
+    </main>
+  );
+}
